94cf24b70957babd6faecdfe9f21a928224fa78f
Konrad Miekus <miekus.konrad@gmail.com>
2022-04-17 12:12:44 +0200

    concurrent mode

--- src/Rect.js
+++ src/Rect.js
@@ -34,25 +34,84 @@ const createElement = (type, props, ...children) => {
   return createFinalElement(elType, elProps, elChildren)
 }
 
-const render = (element, container) => {
+const createDom = fiber => {
   const dom =
-    element.type === 'TEXT_ELEMENT'
+    fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
-  const isProperty = key => key !== 'children'
-  Object.keys(element.props)
-    .filter(isProperty)
+  const isNotChildren = key => key !== 'children'
+  Object.keys(fiber.props)
+    .filter(isNotChildren)
     .forEach(name => {
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach(child =>
-    render(child, dom),
-  )
-
-  container.appendChild(dom)
+  return dom
 }
+
+const performUnitOfWork = fiber => {
+  // a fiber corresponds to one element to be rendered
+  // Each fiber has a link to its first child, its next sibling and its parent
+  //
+  // performUnitOfWork should return nextUnitOfWork
+  // it returns:
+  // - if it has the child => return the child
+  // - if it doesn't have the child and has the sibling => return the sibling
+  // - if it doesn't have the child or the sibling => return the sibling of the parent
+  if (!fiber.dom) fiber.dom = createDom(fiber)
+  if (fiber.parent) fiber.parent.dom.appendChild(fiber.dom)
+
+  const { children } = fiber.props
+  let index = 0
+  let prevSibling = null
+
+  // create fibers for each child
+  while (index < children.length) {
+    const element = children[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) fiber.child = newFiber // set the reference to the first child
+    else prevSibling.sibling = newFiber
+
+    prevSibling = newFiber
+    index++
+  }
+
+  if (fiber.child) return fiber.child
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
+  }
+}
+
+let nextUnitOfWork = null
+
+const render = (element, container) => {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+}
+
+const workLoop = deadline => {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
 
 const Rect = {
   createElement,
